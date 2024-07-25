a9c9a2dc21e409f145db1a1fac18476e6e3913de
chigorgor <chigozie.asadu@gmsil.com>
2024-07-25 18:40:56 +0100

    add nav graph routes

--- app/src/main/java/com/chichi/productlistapp/util/Constants.kt
+++ app/src/main/java/com/chichi/productlistapp/util/Constants.kt
@@ -3,7 +3,9 @@ package com.chichi.productlistapp.util
 object Constants {
 
 
-    const val PRODUCT_DATABASE = "product_database_tale"
+    const val PRODUCT_DATABASE = "product_database_table"
+    const val HOME_SCREEN_ROUTE = "home_screen"
+    const val PRODUCT_SCREEN_ROUTE = "product_screen"
 
     const val ITEMS_PER_PAGE = 10
 
