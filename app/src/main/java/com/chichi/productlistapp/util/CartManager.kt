bdfc0eb41692dffd7dcf4b5e239529a894f22bb0
chigorgor <chigozie.asadu@gmsil.com>
2024-07-26 11:05:38 +0100

    added method to manage cart state and perform calculations

--- app/src/main/java/com/chichi/productlistapp/util/CartManager.kt
+++ app/src/main/java/com/chichi/productlistapp/util/CartManager.kt
@@ -1,5 +1,6 @@
 package com.chichi.productlistapp.util
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.chichi.productlistapp.model.Product
@@ -17,12 +18,14 @@ class CartManager @Inject constructor() {
         } else {
             cartActions.add(product.copy(selectedQty = product.selectedQty))
         }
+        computeTotalAmount()
         return cartActions
     }
 
     /** Inserts product to cart, and, updates `selectedQuantity`**/
     fun insertProduct(product: Product): SnapshotStateList<Product> {
         cartActions.add(product.copy(selectedQty = product.selectedQty))
+        computeTotalAmount()
         return cartActions
     }
 
@@ -42,8 +45,31 @@ class CartManager @Inject constructor() {
                 cartActions.removeAt(index)
             }
         }
+        computeTotalAmount()
         return cartActions
     }
 
+    /** Calculate total amaount and total selected quantity
+     * **/
+    private fun computeTotalAmount(): Pair<Double, Int> {
+        var totalAmount = 0.0
+        var totalQuantity = 0
+
+        cartActions.forEach {
+            val totalSelectedQuantity = it.selectedQty
+            val totalItemPrice = it.price ?: 0
+
+            totalAmount += totalSelectedQuantity * totalItemPrice
+            totalQuantity += totalSelectedQuantity
+        }
+
+        Log.d("TOTAL_AMOUNT_TAG", "computeTotalAmount: $totalAmount ::: $totalQuantity")
+        return Pair(totalAmount, totalQuantity)
+    }
+    private fun logCartActions() {
+        cartActions.forEach {
+            Log.d("CLICKTAG33", "Current Cart Actions: ${it.id} ?? ${it.name} // ${it.selectedQty}")
+        }
+    }
 
 }
